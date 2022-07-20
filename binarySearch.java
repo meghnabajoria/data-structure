d13b12c78e5180a66e1752ac5e12b64983153840
Meghna Bajoria <50470784+meghnabajoria@users.noreply.github.com>
2022-07-20 16:43:32 -0500

    Binary search implementation in java

---
+++ binarySearch.java
@@ -0,0 +1,41 @@
+// Binary search implementation to find an element
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Scratch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(("enter size of array"));
+        Integer sizeOfArray = sc.nextInt();
+
+        System.out.println(("enter your array"));
+        List<Integer> array = new ArrayList<>(sizeOfArray);
+        for (Integer i = 0; i < sizeOfArray; i++) {
+            Integer input = sc.nextInt();
+            array.add(input);
+        }
+        System.out.println("Enter element to be searched");
+        Integer x = sc.nextInt();
+        System.out.println("Your element is at : " + findElementUsingBinarySearch(array, sizeOfArray, x));
+    }
+
+    public static Integer findElementUsingBinarySearch(List<Integer> array, Integer sizeOfArray, Integer x) {
+        Integer low = 0;
+        Integer high = sizeOfArray - 1;
+
+        while (low <= high) {
+            Integer mid = ( low + high ) / 2;
+            if(array.get(mid).compareTo(x) == 0) {
+                return mid;
+            }
+            else if(array.get(mid).compareTo(x) < 0) {
+                low = mid + 1;
+            }
+            else if(array.get(mid).compareTo(x) > 0) {
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
+}
