858410a08e6183e9b4414ed4cc0cc413a3605b2c
Meghna Bajoria <meghnabajoria8@gmail.com>
2022-11-10 12:46:56 -0800

    Update codingNinja_Ninja’s Training.java

--- codingNinja_Ninja’s Training.java
+++ codingNinja_Ninja’s Training.java
@@ -28,7 +28,40 @@ public class Solution {
                 }
             }
             return max;
+        }
+}
 
+//MEMOIZATION
+
+import java.util.*;
+public class Solution {
+        public static int ninjaTraining(int n, int points[][]) {
+            int[][] dp = new int [points.length][points[0].length + 1];
+            for(int i=0;i<points.length;i++)
+                Arrays.fill(dp[i], -1);
+            int ans = recursion(points, n-1, 3, dp);
+            return ans;
         }
 
+        private static int recursion(int points[][], int day, int activity, int[][] dp) {
+            // base condition
+            if(day == 0) {
+                int max = 0;
+                for(int i=0;i<points[0].length;i++) {
+                    if(i != activity)
+                        max = Math.max(max, points[day][i]);
+                }
+                return max;
+            }
+            
+            if(dp[day][activity] != -1) return dp[day][activity];
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i<points[0].length;i++) {
+                if(i != activity) {
+                    int score = points[day][i] + recursion(points,day-1,i, dp);
+                    max = Math.max(max, score);
+                }
+            }
+            return dp[day][activity] = max;
+        }
 }
