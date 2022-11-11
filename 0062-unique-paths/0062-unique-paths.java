4905012e7d65994a5a727e3151e69e4b542eff1e
Meghna Bajoria <meghnabajoria8@gmail.com>
2022-11-10 22:41:11 -0800

    Time: 1 ms (73.98%), Space: 40.7 MB (59.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,31 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++)
-            Arrays.fill(dp[i], -1);
-        return recursion(m-1, n-1, 0, 0, dp);
-    }
     
-    private int recursion(int m, int n, int row, int col, int[][] dp) {
-        if(row == m && col == n) {
-            return 1;
-        }
-        
-        if(row >m || col > n) {
-            return 0;
-        }
-        if(dp[row][col] != -1) return dp[row][col];
-        int down = recursion(m,n, row+1, col,dp);
-        int right = recursion(m,n,row, col+1,dp);
-        return dp[row][col] = down+right;
+     public int uniquePaths(int m, int n) {
+        int[] prev = new int[n];
+         
+         for(int i=0;i<m;i++) {
+             int[] temp = new int[n];
+             for(int j=0;j<n;j++) {
+                 if(i==0 && j==0) {
+                     temp[j] = 1;
+                     continue;
+                 }
+                 
+                 int up = 0;
+                 int left = 0;
+                  if(i>0) {
+                     up = prev[j];
+                 }
+                 if(j>0) {
+                     left = temp[j-1];
+                 }
+                     
+                temp[j] = up+left;
+                 
+             }
+             prev = temp;
+         }
+         
+         return prev[n-1];
     }
 }
