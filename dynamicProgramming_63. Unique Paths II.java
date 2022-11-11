484a00d4e8323d5aced5fcdda3fcd0a1ae352e2f
Meghna Bajoria <meghnabajoria8@gmail.com>
2022-11-11 11:26:25 -0800

    Update dynamicProgramming_63. Unique Paths II.java

--- dynamicProgramming_63. Unique Paths II.java
+++ dynamicProgramming_63. Unique Paths II.java
@@ -89,3 +89,39 @@ class Solution {
         return dp[m-1][n-1];
     }
 }
+
+
+// SPACE OPTIMIZATION
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] prev = new int[m];
+        for(int i=0;i<m;i++) {
+            int[] curr = new int[n];
+            for(int j=0;j<n;j++) {
+                if(grid[i][j] == 1) {
+                    curr[j] = 0;
+                    continue;
+                }
+                else if(i == 0 && j == 0) {
+                    curr[j] = 1;
+                    continue;
+                }
+                
+                int up = 0;
+                int left = 0;
+                if(i-1>=0) {
+                    up = prev[j];
+                }
+                if(j-1>=0) {
+                    left = curr[j-1]; 
+                }
+                curr[j] = up + left;
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+    }
+}
