d5163ea16d4fb90a35367610b0f987f6c4f5c5a3
Meghna Bajoria <meghnabajoria8@gmail.com>
2022-11-11 13:02:21 -0800

    Time: 11 ms (30.50%), Space: 46.6 MB (8.62%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,33 +1,36 @@
 class Solution {
     int min = Integer.MAX_VALUE;
     public int minPathSum(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
-        for(int i=0;i<grid.length;i++)
-            Arrays.fill(dp[i], -1);
-        int max = recursion(grid, 0,0, dp);
-        return max;
-    }
-    
-    private int recursion(int[][] grid, int row, int col, int[][] dp) {
-        if(row == grid.length - 1 && col == grid[0].length - 1) {
-            return grid[grid.length-1][grid[0].length-1];
-        }
+        int n = grid.length;
+        int m = grid[0].length;
         
-        int down = 0;
-        int right = 0;
-        
-        if(dp[row][col] != -1) return dp[row][col];
-        
-        if(row+1<grid.length)
-            down  = grid[row][col] + recursion(grid, row + 1, col, dp);
-        else
-            down = Integer.MAX_VALUE;
-        
-        if(col + 1 < grid[0].length)
-            right = grid[row][col] + recursion(grid, row, col + 1, dp);
-        else
-            right = Integer.MAX_VALUE;
-        
-        return  dp[row][col] = Math.min(down, right);
+        int[][] dp = new int[n][m];
+        // for(int i=0;i<grid.length;i++)
+        //     Arrays.fill(dp[i], -1);
+       
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                //int[] curr = new int[m];
+                if(i == 0 && j == 0) {
+                    dp[i][j] = grid[0][0];
+                    continue;
+                }
+                
+                int up = 0;
+                int left =0;
+                
+                if(i>0)
+                    up = grid[i][j] + dp[i-1][j];
+                else
+                    up = Integer.MAX_VALUE;
+                
+                if(j>0)
+                    left = grid[i][j] + dp[i][j-1];
+                else
+                    left = Integer.MAX_VALUE;
+                dp[i][j] = Math.min(up,left);
+            }
+        } 
+        return dp[n-1][m-1];
     }
 }
