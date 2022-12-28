80fc1e44abd328ef1ffa228d54f0aa6b79b3e828
Meghna Bajoria <meghnabajoria8@gmail.com>
2022-12-27 20:23:37 -0800

    Time: 1 ms (98.08%), Space: 41.9 MB (59.77%) - LeetHub

---
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxScoreWords(String[] words, char[] letters, int[] score) {
+    int[] letterCounts = new int[26];
+    for (char letter : letters)
+       letterCounts[letter - 'a']++;
+
+    return backtrack(words, 0, letterCounts, score);
+}
+
+private int backtrack(String[] words, int start, int[] letterCounts, int[] score) {
+    if (start == words.length)
+       return 0;
+
+    int currScore = 0;
+    int wordScore = 0;
+    int[] nextCounts = letterCounts.clone();
+    for (int i = 0; i < words[start].length(); ++i) {
+       int idx = words[start].charAt(i) - 'a';
+       if (nextCounts[idx] == 0) {
+           wordScore = -1;
+           break;
+       }
+       wordScore += score[idx];
+       nextCounts[idx]--;
+    }
+
+    if (wordScore > 0)
+       currScore = backtrack(words, start + 1, nextCounts, score) + wordScore;
+    currScore = Math.max(currScore, backtrack(words, start + 1, letterCounts, score));
+
+    return currScore;
+    }
+}
