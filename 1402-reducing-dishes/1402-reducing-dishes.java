b620381eab183468a0176e5272cf7a72735af204
Meghna Bajoria <meghnabajoria8@gmail.com>
2022-12-28 19:26:05 -0800

    Time: 16 ms (32.62%), Space: 42.5 MB (41.85%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+        int[][] dp = new int[satisfaction.length+1][satisfaction.length+1];
+        for(int i=0;i<satisfaction.length;i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
+        int max = recursion(0, 1, satisfaction, dp);
+        return max;
+    }
+    
+    private int recursion(int index, int time, int[] satisfaction, int[][] dp) {
+        if(index >= satisfaction.length)
+            return 0;
+       
+        if(dp[index][time] != Integer.MIN_VALUE) {
+            return dp[index][time];
+        }
+        
+        int pick = (satisfaction[index] * time) + 
+            recursion(index+1, time + 1, satisfaction, dp);
+        
+        // dont pick
+        int notPick = recursion(index+1, time, satisfaction, dp);
+        return dp[index][time] = Math.max(pick, notPick);
+    }
+}
+
+// RECURSION
+// class Solution {
+    
+//     public int maxSatisfaction(int[] satisfaction) {
+//         Arrays.sort(satisfaction);
+        
+//         int max = recursion(0, 1, satisfaction);
+//         return max;
+//     }
+    
+//     private int recursion(int index, int time, int[] satisfaction) {
+//         if(index >= satisfaction.length) {
+//             return 0;
+//         }
+       
+//         int pick = (satisfaction[index] * time) + recursion(index+1, time + 1, satisfaction);
+        
+//         // dont pick
+//         int notPick = recursion(index+1, time, satisfaction);
+//         return Math.max(pick, notPick);
+//     }
+// }
