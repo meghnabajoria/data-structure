782013ecf27c293c075bbe8cd840c56bca355b5c
Meghna Bajoria <meghnabajoria8@gmail.com>
2022-12-28 19:57:02 -0800

    Time: 8 ms (47.21%), Space: 42.3 MB (47.85%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,15 +1,40 @@
 class Solution {
     public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
+        int max = solveTabulation(satisfaction);
+        return max;
+    }
+    
+    private int solveTabulation(int[] satisfaction) {
+        int[][] dp = new int[satisfaction.length+1][satisfaction.length+1];
+        for(int i=0;i<satisfaction.length;i++)
+            Arrays.fill(dp[i], 0);
+        
+        for(int index=satisfaction.length-1; index>=0; index--) {
+            for(int time=index; time>=0; time--) {
+                
+                int pick = (satisfaction[index] * (time+1)) + dp[index+1][time+1];
+                int notPick = dp[index+1][time];
+                dp[index][time] = Math.max(pick, notPick);
+                
+            }
+        }
+        return dp[0][0];
+    }
+}
+//MEMOIZATION
+/*class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
         int[][] dp = new int[satisfaction.length+1][satisfaction.length+1];
         for(int i=0;i<satisfaction.length;i++) {
             Arrays.fill(dp[i], Integer.MIN_VALUE);
         }
-        int max = recursion(0, 1, satisfaction, dp);
+        int max = recursionMem(0, 1, satisfaction, dp);
         return max;
     }
     
-    private int recursion(int index, int time, int[] satisfaction, int[][] dp) {
+    private int recursionMem(int index, int time, int[] satisfaction, int[][] dp) {
         if(index >= satisfaction.length)
             return 0;
        
@@ -18,33 +43,33 @@ class Solution {
         }
         
         int pick = (satisfaction[index] * time) + 
-            recursion(index+1, time + 1, satisfaction, dp);
+            recursionMem(index+1, time + 1, satisfaction, dp);
         
         // dont pick
-        int notPick = recursion(index+1, time, satisfaction, dp);
+        int notPick = recursionMem(index+1, time, satisfaction, dp);
         return dp[index][time] = Math.max(pick, notPick);
     }
-}
+}*/
 
 // RECURSION
-// class Solution {
+/*class Solution {
     
-//     public int maxSatisfaction(int[] satisfaction) {
-//         Arrays.sort(satisfaction);
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
         
-//         int max = recursion(0, 1, satisfaction);
-//         return max;
-//     }
+        int max = recursion(0, 1, satisfaction);
+        return max;
+    }
     
-//     private int recursion(int index, int time, int[] satisfaction) {
-//         if(index >= satisfaction.length) {
-//             return 0;
-//         }
+    private int recursion(int index, int time, int[] satisfaction) {
+        if(index >= satisfaction.length) {
+            return 0;
+        }
        
-//         int pick = (satisfaction[index] * time) + recursion(index+1, time + 1, satisfaction);
+        int pick = (satisfaction[index] * time) + recursion(index+1, time + 1, satisfaction);
         
-//         // dont pick
-//         int notPick = recursion(index+1, time, satisfaction);
-//         return Math.max(pick, notPick);
-//     }
-// }
+        // dont pick
+        int notPick = recursion(index+1, time, satisfaction);
+        return Math.max(pick, notPick);
+    }
+}*/
