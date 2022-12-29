0df052bc78a9584dbcd155be86bf7b60fd659f71
Meghna Bajoria <meghnabajoria8@gmail.com>
2022-12-28 19:57:37 -0800

    Tabulation

--- dynamicProgramming_1402. Reducing Dishes.java
+++ dynamicProgramming_1402. Reducing Dishes.java
@@ -52,3 +52,29 @@ class Solution {
         return dp[index][time] = Math.max(pick, notPick);
     }
 }
+
+//TABULATION
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+        int max = solveTabulation(satisfaction);
+        return max;
+    }
+    
+    private int solveTabulation(int[] satisfaction) {
+        int[][] dp = new int[satisfaction.length+1][satisfaction.length+1];
+        for(int i=0;i<satisfaction.length;i++)
+            Arrays.fill(dp[i], 0);
+        
+        for(int index=satisfaction.length-1; index>=0; index--) {
+            for(int time=index; time>=0; time--) {
+                
+                int pick = (satisfaction[index] * (time+1)) + dp[index+1][time+1];
+                int notPick = dp[index+1][time];
+                dp[index][time] = Math.max(pick, notPick);
+                
+            }
+        }
+        return dp[0][0];
+    }
+}
