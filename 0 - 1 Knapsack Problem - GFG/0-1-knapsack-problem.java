2e015ac1d8bf325634db13a4f336abe01a7b1be7
Meghna Bajoria <meghnabajoria8@gmail.com>
2023-02-28 22:02:49 -0800

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -51,28 +51,45 @@ class Solution
     //Function to return max value that can be put in knapsack of capacity W.
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
-         // your code here
-         int[][] dp = new int[n][W+1];
-         for(int i=0;i<n;i++)
-            Arrays.fill(dp[i], -1);
-         return recursion(0, wt, val, W, n, dp);
+int dp[][] = new int[n][W+1];
+    
+    //Base Condition
+    
+    for(int i=wt[0]; i<=W; i++){
+        dp[0][i] = val[0];
+    }
+    
+    for(int ind =1; ind<n; ind++){
+        for(int cap=0; cap<=W; cap++){
+            
+            int notTaken = 0 + dp[ind-1][cap];
+            
+            int taken = Integer.MIN_VALUE;
+            if(wt[ind] <= cap)
+                taken = val[ind] + dp[ind-1][cap - wt[ind]];
+                
+            dp[ind][cap] = Math.max(notTaken, taken);
+        }
+    }
+    
+    return dp[n-1][W];
     } 
     
-    private static int recursion(int index, int[] wt, int[] val, int W, int n, int[][] dp) {
-        if(index >= n || W==0)
-            return 0;
+    // private static int recursion(int index, int[] wt, int[] val, int W, int n, int[][] dp) {
+    //     if(index >= n || W==0)
+    //         return 0;
             
-        // take
-        if(dp[index][W] != -1)
-            return dp[index][W];
-        int len = 0;
-        if(wt[index] <= W)
-            len = val[index] + recursion(index+1, wt, val, W - wt[index], n, dp);
+    //     // take
+    //     if(dp[index][W] != -1)
+    //         return dp[index][W];
+    //     int len = 0;
+    //     if(wt[index] <= W)
+    //         len = val[index] + recursion(index+1, wt, val, W - wt[index], n, dp);
         
-        // not take
-        len = Math.max(len, 0 + recursion(index+1, wt, val, W, n, dp));
-        return dp[index][W] = len;
-    }
+    //     // not take
+    //     len = Math.max(len, 0 + recursion(index+1, wt, val, W, n, dp));
+    //     return dp[index][W] = len;
+    // }
 }
 
 
